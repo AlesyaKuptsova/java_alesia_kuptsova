f5ab70c5010cdde18542013fdaf6a2f4c3b6144a
Alesya_Kuptsova <alesya.kuptsova@tut.by>
2015-10-26 18:31:50 +0300

    module3_java.part1

---
+++ lab2/src/org/lab2/Matrix.java
@@ -0,0 +1,41 @@
+/*
+Transpose a matrix.
+*/
+package org.lab2;
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ * Created by Alesia Kuptsova on 10/25/2015.
+ */
+
+public class Matrix {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.println("Enter rows and columns:");
+        int n = in.nextInt();
+        int m = in.nextInt();
+        in.close();
+        Random randomGenerator = new Random();
+        int[][] mat = new int[n][m];
+        for (int i = 0; i < mat.length; ++i) {
+            for (int j = 0; j < mat[i].length; ++j) {
+                mat[i][j] = randomGenerator.nextInt(10);
+            }
+        }
+        for (int i = 0; i < mat.length; ++i) {
+            for (int j = 0; j < mat[i].length; ++j) {
+                System.out.print(mat[i][j] + " ");
+            }
+            System.out.println();
+        }
+        System.out.println("\n");
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                System.out.print(mat[j][i] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ lab3/src/org/lab3/Sort.java
@@ -0,0 +1,41 @@
+/*
+Sort list or array by count of vowels in string
+*/
+package org.lab3;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+/**
+ * Created by Alesia Kuptsova on 10/25/2015.
+ */
+public class Sort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter string:");
+        String str = sc.nextLine();
+        sc.close();
+        String[] n = str.split(" ");
+        Arrays.sort(n, new Comparator<String>() {
+            @Override
+            public int compare(String o1, String o2) {
+                return countVo(o1) - countVo(o2);
+            }
+        });
+        for (int i = 0; i < n.length; ++i) {
+            System.out.println(n[i]);
+        }
+    }
+
+    private static int countVo(String s) {
+        String vowels = "aeiouAEIOU";
+        int m = 0;
+        for (int i = 0; i < s.length(); ++i) {
+            if (vowels.indexOf(s.charAt(i)) != -1) {
+                m += 1;
+            }
+        }
+        return m;
+    }
+}
